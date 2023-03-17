2363e7a8cd86c006143ab0d6474f124b795d1779
Tushar Choudhari <choudharit23@gmail.com>
2023-03-17 13:49:12 +0530

    :art: Tilt animation for the cards

---
+++ src/lib/CountryCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { tilt } from "../utils/tilt";
+
+
+    let thisCard: undefined | HTMLDivElement = undefined;
+
+</script>
+
+<div 
+    class="card"
+    bind:this={thisCard}
+    use:tilt
+>
+            <slot />
+</div>
+
+<style lang="postcss">
+    .card {
+        @apply w-fit bg-neutral-800 rounded-xl;
+        transform-style: preserve-3d;
+        transform: perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1,1,1);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<script>
+	import CountryCard from "$lib/CountryCard.svelte";
+
+</script>
+
+<div class="p-4">
+  <CountryCard>
+    <button class="w-[310px] h-[400px] flex items-center justify-center border border-solid border-neutral-300 rounded-xl">
+      Card
+    </button>
+  </CountryCard>
+</div>
   
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style>
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
