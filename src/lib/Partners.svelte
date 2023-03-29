6ab5f6e2fc2a89dfe841fa754372e6246ffda7b6
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-29 23:56:35 +0530

    replaced logos with svg

--- src/lib/Partners.svelte
+++ src/lib/Partners.svelte
@@ -1,13 +1,13 @@
 <script>
-	import wormhole from '../assets/logos/wormhole.png';
-	import pyth from '../assets/logos/pyth.png';
-	import foundation from '../assets/logos/foundation.png';
-	import backpack from '../assets/logos/backpack.png';
-	import ftx from '../assets/logos/ftx.png';
+	import wormhole from '../assets/logos/wormhole.svg';
+	import pyth from '../assets/logos/pyth.svg';
+	import foundation from '../assets/logos/foundation.svg';
+	import backpack from '../assets/logos/backpack.svg';
+	import ftx from '../assets/logos/ftx.svg';
 </script>
 
 <div class="partners-container col-span-5 w-full flex flex-col items-center">
-	<h2 class="text-center text-white text-xl font-semibold mb-4">partners</h2>
+	<h2 class="partners-text text-center text-white text-xl font-semibold mb-4">partners</h2>
 	<div class="logos-container flex mt-6 justify-center space-x-4 gap-6">
 		<img class="logo wormhole" src={wormhole} width={'182px'} alt="Wormhole" />
 		<img class="logo pyth" src={pyth} alt="Pyth" />
@@ -18,6 +18,14 @@
 </div>
 
 <style>
+	.partners-text {
+		font-style: normal;
+		font-weight: 700;
+		font-size: 24px;
+		line-height: 26px;
+		text-align: center;
+		letter-spacing: -0.04em;
+	}
 	.partners-container {
 		margin-top: 108px;
 		margin-bottom: 71px;
