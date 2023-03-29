a4cb2916b04d3251fbc87010e1b5a3be12cf870b
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-29 21:02:38 +0530

    partners complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,24 @@
 <script>
-	import CountryCard from "$lib/CountryCard.svelte";
-	import Geographies from "$lib/Geographies.svelte";
-	import Hero from "$lib/Hero.svelte";
-	import Testimonials from "$lib/Testimonials.svelte";
-
+	import CountryCard from '$lib/CountryCard.svelte';
+	import Geographies from '$lib/Geographies.svelte';
+	import Hero from '$lib/Hero.svelte';
+	import Partners from '$lib/Partners.svelte';
+	import Testimonials from '$lib/Testimonials.svelte';
 </script>
-
 
 <Hero />
 <Geographies />
-<div class="pb-[200px]"></div>
-  
-  <!-- <CountryCard>
+<!-- <div class="pb-[200px]"></div> -->
+<Partners />
+
+<!-- <CountryCard>
     <div class="shine"></div>
     <div class="glare"></div>
     <div class="card-content">
         Card
     </div>
   </CountryCard> -->
-  <!-- <Testimonials /> -->
-  
-<style lang="postcss">
+<!-- <Testimonials /> -->
 
+<style lang="postcss">
 </style>
