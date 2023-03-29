1bf8b3e73b44d709dc6a75a2bc9fff3a38561cdf
Tushar Choudhari <choudharit23@gmail.com>
2023-03-29 16:34:38 +0530

    :art: Responsive hero

--- src/lib/common/Header.svelte
+++ src/lib/common/Header.svelte
@@ -6,7 +6,7 @@
 
 <header class="w-full col-span-5 flex items-center justify-between mt-8">
     <NamedLogo />
-    <div class="flex gap-6 items-center">
+    <div class="hidden md:flex gap-6 items-center">
         <a href="/projects" class="text-white font-secondary font-bold text-[14px]">Projects</a>
         <PrimaryButton>
             Collab with us
