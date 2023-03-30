c1354eeeb1604138e2948cf72f0481c83b92c88f
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-30 12:58:17 +0530

    css migrated to tailwind

--- src/lib/SuperteamIRL.svelte
+++ src/lib/SuperteamIRL.svelte
@@ -4,11 +4,13 @@
 	import uniprogram from '../assets/cards/uniprogram.png';
 </script>
 
-<div class="irl-container mt-48 col-span-5">
-	<div class="irl-header">
-		<p class="irl">Superteam IRL</p>
+<div class="mt-48 col-span-5 flex flex-col justify-center items-center">
+	<div class="text-center">
+		<p class="font-normal font-semibold text-3xl leading-none text-white tracking-tighter">
+			Superteam IRL
+		</p>
 	</div>
-	<div class="irl-pix col-span-5 justify-center gap-10 mt-4">
+	<div class="col-span-5 flex justify-center gap-10 mt-4">
 		<img src={buildstations} alt="" />
 		<img src={supermeets} alt="" class="mt-14" />
 		<img src={uniprogram} alt="" />
@@ -16,24 +18,7 @@
 </div>
 
 <style>
-	.irl-container {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-	}
-	.irl-pix {
-		display: flex;
-		flex-direction: row;
-	}
 	.irl {
-		font-family: inherit;
-		font-style: normal;
-		font-weight: 500;
-		font-size: 36px;
-		line-height: 49px;
-		text-align: center;
 		letter-spacing: -0.04em;
-
-		color: #ffffff;
 	}
 </style>
