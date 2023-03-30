171bc202bfeccc1290699bffdde1d28dc3085c9a
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-31 02:41:37 +0530

    production and superteamirl fixage attempt

--- src/lib/Partners.svelte
+++ src/lib/Partners.svelte
@@ -7,11 +7,7 @@
 </script>
 
 <div class="partners-container col-span-5 w-full flex flex-col items-center mt-24">
-	<h2
-		class="partners-text text-center text-white text-xl font-bold mb-4 tracking-tighter mr-[71px] ml-[71px]"
-	>
-		partners
-	</h2>
+	<h2 class="partners-text text-center text-white mb-4 mr-[71px] ml-[71px]">partners</h2>
 	<div class="logos-container flex flex-wrap justify-center items-center mt-6 gap-11">
 		<img class="logo wormhole w-36 h-8" src={wormhole} alt="Wormhole" />
 		<img class="logo pyth w-24 h-8" src={pyth} alt="Pyth" />
@@ -20,3 +16,16 @@
 		<img class="logo ftx w-24 h-6" src={ftx} alt="FTX" />
 	</div>
 </div>
+
+<style>
+	.partners-text {
+		font-style: normal;
+		font-weight: 700;
+		font-size: 24px;
+		line-height: 26px;
+		text-align: center;
+		letter-spacing: -0.04em;
+
+		color: #ffffff;
+	}
+</style>
