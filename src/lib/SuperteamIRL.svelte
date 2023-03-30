35cee6eab4927be57b6a2c5ae6edccf40700ad3c
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-30 11:27:11 +0530

    superteam-irl layout complete

---
+++ src/lib/SuperteamIRL.svelte
@@ -0,0 +1,39 @@
+<script>
+	import buildstations from '../assets/cards/buildstations.png';
+	import supermeets from '../assets/cards/supermeets.png';
+	import uniprogram from '../assets/cards/uniprogram.png';
+</script>
+
+<div class="irl-container mt-48 col-span-5">
+	<div class="irl-header">
+		<p class="irl">Superteam IRL</p>
+	</div>
+	<div class="irl-pix col-span-5 justify-center gap-10 mt-4">
+		<img src={buildstations} alt="" />
+		<img src={supermeets} alt="" class="mt-14" />
+		<img src={uniprogram} alt="" />
+	</div>
+</div>
+
+<style>
+	.irl-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+	.irl-pix {
+		display: flex;
+		flex-direction: row;
+	}
+	.irl {
+		font-family: inherit;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 36px;
+		line-height: 49px;
+		text-align: center;
+		letter-spacing: -0.04em;
+
+		color: #ffffff;
+	}
+</style>
