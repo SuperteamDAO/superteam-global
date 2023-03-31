bcb433ac5c0b8e74db1d7c64ddb6080e25bb3c00
Tushar Choudhari <choudharit23@gmail.com>
2023-03-31 11:06:12 +0530

    Correct way to stick the canvas

---
+++ src/lib/webgl/Scene.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { Canvas } from '@threlte/core'
+	import type Scrollbar from 'smooth-scrollbar';
+	import { onMount, getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
+
+    export let size = { width: 0, height: 0 }
+
+    const scrollbar = getContext<Writable<Scrollbar | null>>('smoothScroll');
+
+    let wrapper: HTMLElement | null = null;
+
+    $: y = 0;
+
+
+    $: {
+        if ($scrollbar) {
+            $scrollbar.addListener(({ offset }) => {
+                if (wrapper) {
+                    y = offset.y
+                }
+            })
+        }
+    }
+</script>
+
+<div 
+    bind:this={wrapper}
+    class="fixed top-0 left-0 z-0"
+    style="height: {size.height}px; width: {size.width}px; transform: translate3d(0, {y}px, 0);"
+>
+    <Canvas {size} >
+    
+    </Canvas>
+</div>
+
+<style lang="postcss">
+    :global(canvas) {
+        @apply w-full h-full;
+	    background: rgb(4, 5, 7);
+    }
+</style>
