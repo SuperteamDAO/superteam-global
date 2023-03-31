e2ce2af75dd2a494db75c8c7a10d268832519fdc
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-31 14:03:45 +0530

    grid added to production component

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,12 @@
+<script></script>
+
+<div
+	class="footer-container relative left-1/2 right-1/2 -translate-x-1/2 w-screen h-[300px] -mt-[90px] col-span-5"
+/>
+
+<style>
+	.footer-container {
+		background-color: gray;
+		z-index: -1;
+	}
+</style>
