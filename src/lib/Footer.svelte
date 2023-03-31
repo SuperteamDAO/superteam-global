d25cdfb6ea108d98a2fbf3d758ade9090db13879
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-31 14:16:53 +0530

    filler cols removed

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,7 @@
 <script></script>
 
 <div
-	class="footer-container relative left-1/2 right-1/2 -translate-x-1/2 w-screen h-[300px] -mt-[90px] col-span-5"
+	class="footer-container relative left-1/2 right-1/2 -translate-x-1/2 w-screen lg:h-[477px] -mt-[90px] col-span-5"
 />
 
 <style>
