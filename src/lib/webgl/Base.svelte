43b7cb556e6662105959ba07a0a48f0c49e74a0c
Tushar Choudhari <choudharit23@gmail.com>
2023-04-05 17:31:01 +0530

    Use scrolltrigger to trigger webgl changes

---
+++ src/lib/webgl/Base.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { OrbitControls, PerspectiveCamera, Canvas, useTexture, T, DirectionalLight } from '@threlte/core'
+
+    export let size = { width: 0, height: 0 }
+    const factor = 5
+
+</script>
+
+
+<T.Group>
+    <PerspectiveCamera
+        fov={50}
+        position={{ z: 100 }}
+    >
+        <!-- <OrbitControls /> -->
+    </PerspectiveCamera>
+    <!-- <DirectionalLight position={{ z: 100 }} color="white" /> -->
+    <T.Mesh position={[0, 0, 0]} receiveShadow>
+        <T.PlaneGeometry args={[size.width / factor, size.height / factor]} />
+        <T.MeshStandardMaterial color="rgb(4, 5, 7)" vertexColors={true} roughness={0.4} />
+    </T.Mesh>
+</T.Group>
