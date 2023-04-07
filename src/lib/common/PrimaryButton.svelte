3d87627a866617a597d9cc8f15d5b1e31335fe0f
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-04-07 14:48:59 +0530

    link added

--- src/lib/common/PrimaryButton.svelte
+++ src/lib/common/PrimaryButton.svelte
@@ -1,13 +1,15 @@
 <script>
-    import clsx from 'clsx';
+	import clsx from 'clsx';
+	export let href;
 </script>
 
-<button 
-    {...$$props} 
-    class={clsx(
-        "px-6 py-3 bg-white text-[#222327] font-secondary font-bold text-[14px] leading-[15px] h-fit rounded-2xl",
-        $$props.class
-    )}
+<a
+	{href}
+	{...$$props}
+	class={clsx(
+		'px-6 py-3 bg-white text-[#222327] font-secondary font-bold text-[14px] leading-[15px] h-fit rounded-2xl',
+		$$props.class
+	)}
 >
-    <slot />
-</button>
+	<slot />
+</a>
