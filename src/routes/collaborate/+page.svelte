8286f26998c5e8e9ee74fcec77c020fbe10b81f2
Tushar Choudhari <choudharit23@gmail.com>
2023-04-07 15:01:14 +0530

    Added collaborate

---
+++ src/routes/collaborate/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import LoveRespect from '$lib/LoveRespect.svelte';
+	import Partners from '$lib/Partners.svelte';
+	import HeroContainer from '$lib/common/HeroContainer.svelte';
+	import CollabServices from '$lib/CollabServices.svelte';
+</script>
+
+<section class="hero col-span-5 sm:h-[466px] md:h-[588px] flex flex-col items-center">
+	<HeroContainer
+		line1="collaborate with the"
+		line2="forefront of web3"
+		line3="superteam is the forefront community for web3,"
+		line4="powered by solana."
+		buttonVisible={false}
+	/>
+</section>
+
+<!-- <div class="pb-[200px]"></div> -->
+<Partners />
+
+<CollabServices />
+
+<LoveRespect collab={true} />
+
+<style lang="postcss">
+</style>
