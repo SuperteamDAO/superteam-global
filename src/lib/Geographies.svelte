06fa968b467bb011d1d2e9a179576b9e6264346a
Tushar Choudhari <choudharit23@gmail.com>
2023-04-08 22:26:19 +0530

    Fix tilt

--- src/lib/Geographies.svelte
+++ src/lib/Geographies.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onMount } from 'svelte';
 	import CountryCard from './CountryCard.svelte';
 	import NavButton from './common/NavButton.svelte';
 	import { CaretLeft, CaretRight } from 'phosphor-svelte';
@@ -6,78 +7,38 @@
 	 * @type {HTMLElement}
 	 */
 	let carousel;
-	let currentPosition = 0;
-	// function scrollLeft() {
-	// 	const minPosition = 0;
-	// 	const scrollAmount = carousel.clientWidth / 2;
-	// 	currentPosition = Math.max(currentPosition - scrollAmount, minPosition);
-	// 	carousel.scrollTo({ left: currentPosition, behavior: 'smooth' });
-	// }
-
-	// function scrollRight() {
-	// 	const maxPosition = carousel.scrollWidth - carousel.clientWidth;
-	// 	const scrollAmount = carousel.clientWidth / 2;
-	// 	currentPosition = Math.min(currentPosition + scrollAmount, maxPosition);
-	// 	carousel.scrollTo({ left: currentPosition, behavior: 'smooth' });
-	// }
-
-
+	
 </script>
+
+<svelte:window />
 
 <div
 	class="carousel-main relative left-1/2 right-1/2 -translate-x-1/2 w-screen overflow-x-visible flex flex-col col-span-5"
 >
-	<!-- <div
-		class="carousel-container flex items-center justify-between text-white mr-[71px] ml-[71px] mb-4"
-	> -->
-		<!-- <div class="nav-buttons w-full md:flex">
-			<NavButton direction="left" class="left hidden md:flex" on:click={scrollLeft}
-				><CaretLeft size={20} /></NavButton
-			>
-			<h2 class="chapters text-center flex-1">chapters</h2>
-			<NavButton direction="right" class="right hidden md:flex" on:click={scrollRight}
-				><CaretRight size={20} /></NavButton
-			>
-		</div> -->
-	<!-- </div> -->
-	<!-- <div
-		bind:this={carousel}
-		class="country-cards-container w-full col-span-5 overflow-x-scroll overflow-y-visible scrollbar-hidden pb-0"
-	>
-		<div class="country-cards w-full flex gap-8 min-[1630px]:justify-center py-12">
-			<CountryCard />
-			<CountryCard />
-			<CountryCard />
-			<CountryCard />
-			<CountryCard />
-		</div>
-	</div> -->
-	<!-- <div class="nav-buttons text-white md:hidden flex justify-center gap-11 mt-4">
-		<NavButton direction="left" class="left" on:click={scrollLeft}
-			><CaretLeft size={20} /></NavButton
-		>
-		<NavButton direction="right" class="right" on:click={scrollRight}
-			><CaretRight size={20} /></NavButton
-		>
-	</div> -->
-	<div bind:this={carousel} class="h-[400px] w-full flex items-center px-6">
-		<CountryCard />
-		<!-- <CountryCard />
+	<!-- <button class="text-white">Left</button> -->
+	<div bind:this={carousel} class="carousel-wrapper h-fit w-full flex items-center px-6 py-8 gap-6 overflow-x-scroll overflow-y-visible scrollbar-hidden">
+		<div class="left-offset"></div>
 		<CountryCard />
 		<CountryCard />
 		<CountryCard />
-		<CountryCard /> -->
+		<CountryCard />
+		<CountryCard />
 	</div>
+	<!-- <button class="text-white">Right</button> -->
 </div>
 
 <style lang="postcss">
-	.chapters {
-		font-family: 'Archivo_SemiExpanded';
-		font-style: normal;
-		font-weight: 600;
-		font-size: 24px;
-		line-height: 26px;
-		text-align: center;
-		color: #ffffff;
+	.left-offset {
+		@apply min-w-[calc((100vw-1176px)/2)];
+	}
+
+	@media (min-width: 1800px) {
+		.left-offset {
+			@apply min-w-0;
+		}
+
+		.carousel-wrapper {
+			@apply justify-center;
+		}
 	}
 </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,12 +1,18 @@
 <script>
 	import ProjectPageHero from "$lib/Projects/ProjectPageHero.svelte";
 	import ProjectsGrid from "$lib/Projects/ProjectsGrid.svelte";
+	import HeroContainer from "$lib/common/HeroContainer.svelte";
     import builbg from '../../assets/bg-build.jpg';
 
 </script>
 
 <img src={builbg} class="absolute top-0 left-0 w-full object-cover" alt="" />
-<section class="relative col-span-5 flex flex-col items-center pb-[300px]">   
-    <ProjectPageHero />
+<section class="relative col-span-5 flex flex-col items-center pb-[300px]">
+    <HeroContainer
+         line1="Industry leading"
+         line2="Projects"
+         line3="superteam is the forefront community for web3,"
+         line4="powered by solana."
+    />
     <ProjectsGrid />
 </section>
