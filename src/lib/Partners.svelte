8611028b9f398a5970731a09d5aed3ca24cbf879
Tushar Choudhari <tushar.choudhari@tickertape.in>
2023-04-10 21:55:59 +0530

    Add shine to the productions

--- src/lib/Partners.svelte
+++ src/lib/Partners.svelte
@@ -8,9 +8,9 @@
 
 <div class="partners-container col-span-5 w-full flex flex-col items-center mt-24">
 	<h2
-		class="partners-text text-center text-white mb-4 mr-[71px] ml-[71px] text-[20px] md:[24px] lg:leading-[26px] leading-[22px]"
+		class="partners-text text-center text-white mb-4 mr-[71px] ml-[71px] text-[20px] md:text-[24x] md:[24px] lg:leading-[26px] leading-[22px]"
 	>
-		partners
+		Partners
 	</h2>
 	<div class="logos-container flex flex-wrap justify-center items-center mt-6 gap-11">
 		<img class="logo wormhole w-36 h-8" src={wormhole} alt="Wormhole" />
