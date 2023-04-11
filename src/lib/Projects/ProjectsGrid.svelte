ea3fb3f9b55e0721b17fcab92903a4cef2bdd652
Tushar Choudhari <tushar.choudhari@tickertape.in>
2023-04-11 23:55:05 +0530

    Wrapping up responsiveness for projects page

--- src/lib/Projects/ProjectsGrid.svelte
+++ src/lib/Projects/ProjectsGrid.svelte
@@ -15,25 +15,50 @@
         }
     }
 
+    const getOffsetClasses = (index) => {
+        if (offsetArray[index] && index % 2 === 0) {
+            return 'mt-0 md:mt-8';
+        }
+
+        if (offsetArray[index] && index % 2 === 1) {
+            return 'mt-0 lg:mt-8';
+        }
+
+        if (!offsetArray[index] && index % 2 === 0) {
+            return 'mt-0 md:mt-8 lg:mt-0';
+        }
+
+        return 'mt-0'
+    }
+
 </script>
 
 <div class="flex justify-center items-center mt-[42px]">
-	<div class="flex flex-col gap-8 lg:grid lg:gap-0 cards-container">
+	<div class="flex flex-col gap-8 md:grid md:gap-0 cards-container">
         {#each projects as { fields }, index}
             <ProjectCard
                 name={fields.Name}
                 tagline={fields.Tagline}
                 projectLink={fields["Project Link"]}
-                class="col-span-1 row-span-1 {offsetArray[index] ? 'lg:mt-8' : ''}" 
+                imgUrl={fields["Logo Link"]}
+                class="col-span-1 row-span-1 {getOffsetClasses(index)}"
             />
         {/each}
 	</div>
 </div>
 
 <style>
-	.cards-container {
-		grid-template-columns: repeat(3, 1fr);
-		grid-template-rows: auto;
-        column-gap: 32px;
-	}
+    @media (min-width: 786px) {
+        .cards-container {
+            grid-template-columns: repeat(2, 1fr);
+            column-gap: 32px;
+        }
+    }
+    @media (min-width: 1024px) {
+        .cards-container {
+            grid-template-columns: repeat(3, 1fr);
+            grid-template-rows: auto;
+        }
+    }
+
 </style>
