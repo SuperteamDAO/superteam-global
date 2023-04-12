6816e4de0fc317fc6b274eb42e57fdc4c3888519
Tushar Choudhari <tushar.choudhari@tickertape.in>
2023-04-12 21:12:54 +0530

    Add stars with css

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -9,23 +9,80 @@
 
 <section class="hero relative col-span-5 h-[calc(100vh-40px-32px)] flex flex-col items-center">
 	<HeroContainer {line1} {line2} {line3} {line4} {buttonVisible} />
-	<div class="hero-bg absolute top-0 left-0 w-full h-full"></div>
+	<div class="hero-bg absolute top-0 left-0 w-full h-full overflow-hidden">
+		<div class="stars"></div>
+		<div class="stars2"></div>
+		<div class="stars3"></div>
+	</div>
 </section>
 
-<style lang="postcss">
+<style lang="scss">
+	$starStartOffset: 600px;
+	$starFieldWidth: 2560;
+	$starFieldHeight: 2560;
+
+	$numStarOneStars: 1700;
+	$numStarTwoStars: 700;
+	$numStarThreeStars: 200;
+
+	$starOneScrollDuration: 100s;
+	$starTwoScrollDuration: 125s;
+	$starThreeScrollDuration: 175s;
+
+	@function create-stars($n) {
+  		$stars: "#{random($starFieldWidth)}px #{random($starFieldHeight)}px #FFF";
+
+		@for $i from 2 through $n {
+			$stars: "#{$stars} , #{random($starFieldWidth)}px #{random($starFieldHeight)}px #FFF";
+		}
+  		@return unquote($stars);
+	}
+
+	@mixin star-template($numStars, $starSize, $scrollSpeed) {
+		z-index: 1;
+		width: $starSize;
+		height: $starSize;
+		border-radius: 50%;
+		background: transparent;
+		box-shadow: create-stars($numStars);
+		animation: animStar $scrollSpeed linear infinite;
+		&:after {
+			content: " ";
+			top: -$starStartOffset;
+			width: $starSize;
+			height: $starSize;
+			border-radius: 50%;
+			position: absolute;
+			background: transparent;
+			box-shadow: create-stars($numStars);
+		}
+	}
+
 	.hero-bg {
 		width: 100%;
 		height: calc(100% + 48px);
 		width: calc(100% + 40px + 40px);
 		transform: translateY(-48px) translateX(-40px);
-		background-image: url('../assets/hero/Hero_L3.png');
-		background-size: cover;
 		z-index: 1;
+
+		.stars {
+			@include star-template($numStarOneStars, 1px, $starOneScrollDuration);
+		}
+		.stars2 {
+			@include star-template($numStarTwoStars, 2px, $starTwoScrollDuration);
+		}
+		.stars3 {
+			@include star-template($numStarThreeStars, 3px, $starThreeScrollDuration);
+		}
 	}
 
 	.hero-bg::after {
 		content: '';
-		@apply absolute top-0 left-0 w-full h-full;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
 		background-image: url('../assets/hero/Hero_L2.png');
 		background-size: cover;
 		background-repeat: no-repeat;
@@ -35,7 +92,11 @@
 
 	.hero-bg::before {
 		content: '';
-		@apply absolute top-0 left-0 w-full h-full;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
 		background-image: url('../assets/hero/Hero_L1.png');
 		background-size: cover;
 		background-repeat: no-repeat;
