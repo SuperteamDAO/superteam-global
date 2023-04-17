91d2cdc3efd8de2f38ff2a62c04fc50eb16ffbf1
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-04-17 17:43:30 +0530

    collab pic added for desktop

--- src/lib/Collab.svelte
+++ src/lib/Collab.svelte
@@ -1,22 +1,22 @@
 <script>
-	import blackhole1 from '../assets/images/blackhole1.png';
+	import collabpic from '../assets/images/collabpic.png';
 	import blackhole2 from '../assets/images/blackhole2.png';
 	import PrimaryButton from './common/PrimaryButton.svelte';
 </script>
 
 <div
-	class="collab-container col-span-5 flex flex-col justify-center align-middle relative left-1/2 right-1/2 -translate-x-1/2 w-screen -mt-[100px] md:-mt-[150px] -mb-[200px] md:-mb-[20px] lg:-mb-[30px] z-1 bg-black h-fit md:h-fit md:rounded-[64px]"
+	class="collab-container col-span-5 flex flex-col justify-center align-middle relative left-1/2 right-1/2 -translate-x-1/2 w-screen -mt-[100px] md:-mt-[150px] -mb-[200px] md:-mb-[20px] lg:-mb-[30px] z-1 bg-black h-fit md:h-fit"
 >
 	<div class="overlay absolute w-full h-full hidden md:block" />
 	<!-- uncomment the following line to add the updated image: -->
-	<!-- <img src={blackhole1} alt="" class="lg:h-fit h-[807px] md:rounded-[64px] hidden md:block" /> -->
+	<img src={collabpic} alt="" class="lg:h-fit h-[807px] md:rounded-[64px] hidden md:block" />
 
 	<!-- placeholder below, remove the following line after updating the image : -->
-	<div class="h-[807px] md:rounded-[64px] hidden md:block" />
+	<!-- <div class="h-[807px] md:rounded-[64px] hidden md:block" /> -->
 
 	<div class="overlay-sm absolute w-full h-full block md:hidden" />
 	<!-- uncomment the following line to add the updated image: -->
-	<!-- <img src={blackhole2} alt="" class="w-full h-[669px] md:rounded-[64px] block md:hidden" /> -->
+	<!-- <img src={collabpic} alt="" class="w-full h-[700px] md:rounded-[64px] block md:hidden" /> -->
 
 	<!-- placeholder below, remove the following line after updating the image : -->
 	<div class="w-full h-[669px] md:rounded-[64px] block md:hidden" />
