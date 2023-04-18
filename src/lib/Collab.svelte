392bef8a7c49aa086a2c6683057cedfede141606
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-04-18 16:12:07 +0530

    collab image added for phone

--- src/lib/Collab.svelte
+++ src/lib/Collab.svelte
@@ -1,14 +1,15 @@
 <script>
 	import collabpic from '../assets/images/collabpic.png';
+	import collabsm from '../assets/images/collab-sm.png';
 	import blackhole2 from '../assets/images/blackhole2.png';
 	import PrimaryButton from './common/PrimaryButton.svelte';
 </script>
 
 <div
-	class="collab-container col-span-5 flex flex-col justify-center align-middle relative left-1/2 right-1/2 -translate-x-1/2 w-screen -mt-[100px] md:-mt-[150px] -mb-[200px] md:-mb-[20px] lg:-mb-[30px] z-1 bg-black h-fit md:rounded-[64px] md:h-fit"
+	class="collab-container col-span-5 flex flex-col justify-center align-middle relative left-1/2 right-1/2 -translate-x-1/2 w-screen -mt-[70px] md:-mt-[150px] -mb-[50px] md:-mb-[20px] lg:-mb-[30px] z-1 bg-black h-fit md:rounded-[64px] md:h-fit"
 >
 	<div class="overlay absolute w-full h-full hidden md:block" />
-	<!-- uncomment the following line to add the updated image: -->
+	<!-- image for large screens: -->
 	<img
 		src={collabpic}
 		loading="lazy"
@@ -19,17 +20,18 @@
 	<!-- placeholder below, remove the following line after updating the image : -->
 	<!-- <div class="h-[807px] md:rounded-[64px] hidden md:block" /> -->
 
-	<div class="overlay-sm absolute w-full h-full block md:hidden" />
-	<!-- uncomment the following line to add the updated image: -->
-	<!-- <img src={collabpic} alt="" class="w-full h-[700px] md:rounded-[64px] block md:hidden" /> -->
+	<!-- uncomment following line for overlay in small devices: -->
+	<!-- <div class="overlay-sm absolute w-full h-full block md:hidden" /> -->
+	<!-- image for small screens: -->
+	<img src={collabsm} alt="" class="w-full h-[518px] object-cover block md:hidden" />
 
 	<!-- placeholder below, remove the following line after updating the image : -->
-	<div class="w-full h-[669px] md:rounded-[64px] block md:hidden" />
+	<!-- <div class="w-full h-[669px] md:rounded-[64px] block md:hidden" /> -->
 
 	<div
 		class="history-stuff w-full flex flex-col md:justify-center items-center h-[669px] md:h-full"
 	>
-		<p class="history mt-[80px] text-[24px] w-[189px] md:text-[32px] md:w-[245px]">
+		<p class="history mt-[145px] md:mt-[80px] text-[24px] w-[189px] md:text-[32px] md:w-[245px]">
 			be a part of history
 		</p>
 		<div class="button mt-[50px]">
