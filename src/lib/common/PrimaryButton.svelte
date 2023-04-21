1ccf17e36261846f9bf8f321d0619a72b1995327
Tushar Choudhari <choudharit23@gmail.com>
2023-04-21 09:16:15 +0530

    Chaneg hover state of the borde in illustrator card

--- src/lib/common/PrimaryButton.svelte
+++ src/lib/common/PrimaryButton.svelte
@@ -7,9 +7,19 @@
 	{href}
 	{...$$props}
 	class={clsx(
-		'px-6 py-3 bg-white text-[#222327] font-secondary font-bold text-[14px] leading-[15px] h-fit rounded-2xl',
+		'primary-cta px-6 py-3 bg-white text-[#222327] font-secondary font-bold text-[14px] leading-[15px] h-fit rounded-2xl',
 		$$props.class
 	)}
 >
 	<slot />
 </a>
+
+<style>
+	.primary-cta:hover {
+		box-shadow: 0px -4px 27px rgba(200, 108, 59, 0), 0px 4px 12px rgba(57, 22, 149, 0);
+		transition: box-shadow 0.25s ease-out;	
+	}
+	.primary-cta:hover {
+		box-shadow: 0px -4px 27px rgba(200, 108, 59, 0.68), 0px 4px 12px rgba(57, 22, 149, 1);
+	}
+</style>
