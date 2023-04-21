e4b7e1701870f4ffa3a6a95d1869fe0a778c7fb8
Tushar Choudhari <choudharit23@gmail.com>
2023-04-21 09:16:15 +0530

    Chaneg hover state of the borde in illustrator card

--- src/lib/common/IllustrationCard.svelte
+++ src/lib/common/IllustrationCard.svelte
@@ -6,25 +6,46 @@
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class={clsx('image-with-text relative rounded-lg cursor-pointer', $$props.class)} on:click>
-	<img src={imageUrl} width={300} height={400} class="rounded-lg" loading="lazy" alt="" />
-	<p
-		class="text-on-image absolute text-left font-bold text-white top-[32px] left-[32px] text-[20px] md:text-[28px]"
-	>
-		{text}
-	</p>
+<div class={clsx('image-with-text rounded-lg cursor-pointer', $$props.class)} on:click>
+	<div class="image-with-text-content w-full h-full relative overflow-hidden rounded-lg">
+		<img src={imageUrl} width={300} height={400} class="rounded-lg absolute z-10" loading="lazy" alt="" />
+		<p
+			class="z-10 text-on-image absolute text-left font-bold text-white top-[32px] left-[32px] text-[20px] md:text-[28px]"
+		>
+			{text}
+		</p>
+	</div>
 </div>
 
 <style>
 	.image-with-text {
-		background: linear-gradient(177.68deg, #0e0e0e -18.67%, #000000 114.79%);
+		position: relative;
+		z-index: 1;
+		background: #202020;
+		padding: 1px;
 		transition: background 0.5s ease;
-
-		border: 1px solid transparent;
 		transition: border 400ms;
 	}
-	.image-with-text:hover {
-		border: 1px solid #202020;
+
+	.image-with-text-content {
+		background: linear-gradient(177.68deg, #0e0e0e -18.67%, #000000 114.79%);
+	}
+
+	.image-with-text::after {
+		content: '';
+		z-index: -1;
+		@apply absolute rounded-lg;
+		top: 0px;
+		left: 0px;
+		width: 100%;
+		height: 100%;
+		background: linear-gradient(144.34deg, #F7F3F3 -29.96%, rgba(63, 63, 63, 0.67) 131.56%);
+		opacity: 0;
+		transition: opacity 0.3s ease;
+	}
+
+	.image-with-text:hover::after {
+		opacity: 1;
 	}
 
 	.text-on-image {
