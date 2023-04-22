95c8df8f4a7dcd2ea53f2d4b03480fe32b6305fe
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-04-23 03:22:56 +0530

    projects bg changed

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,20 +1,19 @@
 <script>
-	import ProjectsGrid from "$lib/Projects/ProjectsGrid.svelte";
-	import HeroContainer from "$lib/common/HeroContainer.svelte";
-    import builbg from '../../assets/bg-build.jpg';
+	import ProjectsGrid from '$lib/Projects/ProjectsGrid.svelte';
+	import HeroContainer from '$lib/common/HeroContainer.svelte';
+	import builbg from '../../assets/images/Build.webp';
 
-    export let data
-
+	export let data;
 </script>
 
 <img src={builbg} class="absolute top-0 left-0 w-full object-cover" loading="eager" alt="" />
 <section class="relative col-span-5 flex flex-col items-center pb-[300px]">
-    <HeroContainer
-         line1="Industry leading"
-         line2="Projects"
-         line3="superteam is the forefront community for web3,"
-         line4="powered by solana."
-         buttonVisible={false}
-    />
-    <ProjectsGrid projects={data.projects} />
+	<HeroContainer
+		line1="Industry leading"
+		line2="Projects"
+		line3="superteam is the forefront community for web3,"
+		line4="powered by solana."
+		buttonVisible={false}
+	/>
+	<ProjectsGrid projects={data.projects} />
 </section>
