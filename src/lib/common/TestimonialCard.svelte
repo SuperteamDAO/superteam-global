28e6dfeafa9d21d8407f42ad1551489318b4d478
Tushar Choudhari <choudharit23@gmail.com>
2023-04-23 18:03:05 +0530

    Optimize twitter card

--- src/lib/common/TestimonialCard.svelte
+++ src/lib/common/TestimonialCard.svelte
@@ -10,28 +10,18 @@
 
 <a href={twturl} class="cursor-pointer" target="_blank">
 	<div class="test-container h-fit flex flex-col items-start py-7 px-6 rounded-[5px]">
-		<div
-			class="font-primary font-medium leading-[-0.04em] text-white lg:text-[18px] md:text-[16px] text-[14px]"
-		>
-			<p>
+			<p class="font-primary font-medium leading-[-0.04em] text-white lg:text-[18px] md:text-[16px] text-[14px]">
 				{content}
 			</p>
-		</div>
 		<div class="flex flex-row mt-[40px]">
-			<div class="image w-[40px] h-[40px] mr-[24px]">
-				<ImageLoader src={imgurl} width={40} height={40} alt="" class="rounded-full" loading="lazy" />
-			</div>
+			<ImageLoader src={imgurl} width={40} height={40} alt="" class="rounded-full w-[40px] h-[40px] mr-[24px]" loading="lazy" />
 			<div class="user">
-				<div
-					class="name text-white text-[12px] lg:text-[16px] md:text-[15px] lg:leading-[17px] md:leading-[16px] leading-[13px]"
+				<p class="font-secondary font-bold text-white text-[12px] lg:text-[16px] md:text-[15px] lg:leading-[17px] md:leading-[16px] leading-[13px]">{name}</p>
+				<p
+					class="username opacity-40 text-white mt-[4px] text-[12px] lg:text-[16px] md:text-[15px] lg:leading-[17px] md:leading-[16px] leading-[13px]"
 				>
-					<p>{name}</p>
-				</div>
-				<div
-					class="username mt-[4px] text-[12px] lg:text-[16px] md:text-[15px] lg:leading-[17px] md:leading-[16px] leading-[13px]"
-				>
-					<p>{username}</p>
-				</div>
+					{username}
+				</p>
 			</div>
 		</div>
 	</div>
@@ -45,12 +35,7 @@
 		border: 1px solid rgba(177, 177, 177, 0.2);
 		border-radius: 5px;
 	}
-	.username {
-		color: #ffffff;
-		opacity: 0.4;
-	}
 	.user {
-		font-family: 'Archivo_SemiExpanded';
 		font-style: normal;
 		font-weight: 700;
 	}
