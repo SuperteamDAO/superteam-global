63bbcd7bc4eac027296f28f3e0514baa608b918d
Tushar Choudhari <choudharit23@gmail.com>
2023-04-24 18:38:50 +0530

    Add stars to the collaborate page

--- src/routes/collaborate/+page.svelte
+++ src/routes/collaborate/+page.svelte
@@ -4,6 +4,9 @@
 	import HeroContainer from '$lib/common/HeroContainer.svelte';
 	import CollabServices from '$lib/CollabServices.svelte';
 	import Collab from '$lib/Collab.svelte';
+	import ImageLoader from '$lib/ImageLoader.svelte';
+
+	import hero from '../../assets/collaborate/collaborate.webp'
 </script>
 
 <div class="col-span-5 h-fit">
@@ -15,7 +18,19 @@
 			line4="powered by solana."
 			buttonVisible={false}
 		/>
-		<div class="hero-bg absolute top-0 left-0 w-full h-[calc(100vh)] overflow-hidden" />
+		<div class="hero-bg absolute top-0 left-0 w-full h-[calc(100vh)] overflow-hidden">
+			<div class="stars"></div>
+			<div class="stars2"></div>
+			<div class="stars3"></div>
+			<div class="w-full h-full relative">
+				<ImageLoader
+					src={hero}
+					loading="eager"
+					alt=""
+					class="object-cover w-full h-full z-10"
+				/>
+			</div>
+		</div>
 	</section>
 
 	<!-- <div class="pb-[200px]"></div> -->
@@ -27,14 +42,90 @@
 	<Collab />
 </div>
 
-<style lang="postcss">
+<style lang="scss">
+	$starStartOffset: 600px;
+	$starFieldWidth: 2560;
+	$starFieldHeight: 2560;
+
+	$numStarOneStars: 500;
+	$numStarTwoStars: 200;
+	$numStarThreeStars: 200;
+
+	$starOneScrollDuration: 100s;
+	$starTwoScrollDuration: 125s;
+	$starThreeScrollDuration: 175s;
+
+	@keyframes animStar {
+		from {
+			transform: translateY(0px);
+		}
+		to {
+			transform: translateY(-#{$starFieldHeight}px)
+		}
+	}
+
+
+	@function create-stars($n) {
+  		$stars: "#{random($starFieldWidth)}px #{random($starFieldHeight)}px #FFF";
+
+		@for $i from 2 through $n {
+			$stars: "#{$stars} , #{random($starFieldWidth)}px #{random($starFieldHeight)}px #FFF";
+		}
+  		@return unquote($stars);
+	}
+
+	@mixin star-template($numStars, $starSize, $scrollSpeed) {
+		z-index: 1;
+		width: $starSize;
+		height: $starSize;
+		border-radius: 50%;
+		background: transparent;
+		box-shadow: create-stars($numStars);
+		animation: animStar $scrollSpeed linear infinite;
+		&:after {
+			content: " ";
+			top: -$starStartOffset;
+			width: $starSize;
+			height: $starSize;
+			border-radius: 50%;
+			position: absolute;
+			background: transparent;
+			box-shadow: create-stars($numStars);
+		}
+	}
+
+	.hero {
+		perspective: 1000px;
+	}
+
 	.hero-bg {
-		background-image: url('../../assets/collaborate/collaborate.webp');
-		background-size: cover;
-		background-position: center;
-		background-repeat: no-repeat;
-		transform: translateY(-48px) translateX(-40px);
+		width: 100%;
+		height: calc(100% + 48px);
 		width: calc(100% + 40px + 40px);
+		transform: translateY(-48px) translateX(-40px);
+		z-index: 1;
+
+		.stars {
+			@include star-template($numStarOneStars, 1px, $starOneScrollDuration);
+		}
+		.stars2 {
+			@include star-template($numStarTwoStars, 2px, $starTwoScrollDuration);
+		}
+		.stars3 {
+			@include star-template($numStarThreeStars, 3px, $starThreeScrollDuration);
+		}
+	}
+
+	.hero-bg::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 5;
+		transform-style: preserve-3d;
+		transform: translateZ(100px);
 	}
 
 	@media (min-width: 768px) {
