3f57af8e40395eaf3c6a9488bb72ae933a73e72f
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-04-25 20:05:25 +0530

    preloaded hero image error fix x2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,8 @@
 	import Partners from '$lib/Partners.svelte';
 	import Production from '$lib/Production.svelte';
 	import SuperteamIrl from '$lib/SuperteamIRL.svelte';
-	import hero05x from '../../assets/hero/hero_home0.5x.webp';
-	import hero from '../../assets/hero/hero_home.webp';
+	import hero05x from '../assets/hero/hero_home0.5x.webp';
+	import hero from '../assets/hero/hero_home.webp';
 	import hero1_5x from '../assets/hero/hero_home1.5x.webp';
 </script>
 
