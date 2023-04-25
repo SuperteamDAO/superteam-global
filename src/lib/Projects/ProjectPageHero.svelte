bbef06d8d6597a1045afed5822bbf0e349c5b282
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-04-25 18:45:47 +0530

    projects bg responsive

--- src/lib/Projects/ProjectPageHero.svelte
+++ src/lib/Projects/ProjectPageHero.svelte
@@ -1,22 +1,26 @@
 <script>
-	import AnimatedLogo from "$lib/common/AnimatedLogo.svelte";
-
+	import AnimatedLogo from '$lib/common/AnimatedLogo.svelte';
 </script>
+
 <div class="mt-[72px] md:mt-[96px] lg:mt[160px] flex flex-col items-center justify-center">
-    <div class="flex items-center justify-center">
-        <AnimatedLogo />
-    </div>
-    <h1 class="heading w-[280px] md:w-[430px] flex flex-col text-[32px] md:text-[48px] leading-[1.005] tracking-[-4%] font-semibold font-secondary text-white mt-12 text-center">
-			<span class="line block">
-				<span> Industry leading </span>
-			</span>
-			<span class="line block">
-				<span> Projects </span>
-			</span>
+	<div class="flex items-center justify-center">
+		<AnimatedLogo />
+	</div>
+	<h1
+		class="heading w-[280px] md:w-[430px] flex flex-col text-[32px] md:text-[48px] leading-[1.005] tracking-[-4%] font-semibold font-secondary text-white mt-12 text-center"
+	>
+		<span class="line block">
+			<span> Industry leading </span>
+		</span>
+		<span class="line block">
+			<span> Projects </span>
+		</span>
 	</h1>
-    <p class="sub-heading opacity-1 text-white font-primary text-[16px] md:text-[20px] leading-[1.35] tracking-[-4%] text-center mt-8">
-			superteam is the forefront community for web3,
-			<br />
-			powered by solana.
+	<p
+		class="sub-heading opacity-1 text-white font-primary text-[16px] md:text-[20px] leading-[1.35] tracking-[-4%] text-center mt-8"
+	>
+		superteam is the forefront community for web3,
+		<br />
+		powered by solana.
 	</p>
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,23 +1,39 @@
 <script>
 	import ProjectsGrid from '$lib/Projects/ProjectsGrid.svelte';
 	import HeroContainer from '$lib/common/HeroContainer.svelte';
-	import builbg from '../../assets/images/Build.webp';
+	import build_lg from '../../assets/images/build-lg.webp';
+	import build_sm from '../../assets/images/build-sm.webp';
+	import build_xl from '../../assets/images/build-xl.webp';
+	import ImageLoader from '$lib/ImageLoader.svelte';
 
 	export let data;
 </script>
 
 <svelte:head>
 	<title>Projects</title>
-	<meta name="description" content="Explore projects Superteam Members around the world have built and launched!">
-	<meta name='og:title' content='Projects'>
-	<meta name='og:type' content='community'>
-	<meta name='og:url' content='https://superteam.fun'>
-	<meta name='application-name' content='superteam'>
-	<meta name='og:email' content='hello@superteam.fun'>
+	<meta
+		name="description"
+		content="Explore projects Superteam Members around the world have built and launched!"
+	/>
+	<meta name="og:title" content="Projects" />
+	<meta name="og:type" content="community" />
+	<meta name="og:url" content="https://superteam.fun" />
+	<meta name="application-name" content="superteam" />
+	<meta name="og:email" content="hello@superteam.fun" />
 	<meta name="twitter:title" content="Projects" />
 </svelte:head>
 
-<img src={builbg} class="absolute top-0 left-0 w-full object-cover" loading="eager" alt="" />
+<!-- <img src={build_lg} class="absolute top-0 left-0 w-full object-cover" loading="eager" alt="" /> -->
+
+<ImageLoader
+	src={build_lg}
+	srcset={`${build_sm} 640w, ${build_lg} 1440w, ${build_xl} 2560w`}
+	sizes="(max-width: 640px) 100vw, (max-width: 1440px) 100vw, 2560px"
+	loading="eager"
+	alt=""
+	class="absolute top-0 left-0 w-full object-cover"
+/>
+
 <section class="relative col-span-5 flex flex-col items-center pb-[300px]">
 	<HeroContainer
 		line1="Industry leading"
@@ -28,3 +44,9 @@
 	/>
 	<ProjectsGrid projects={data.projects} />
 </section>
+
+<style>
+	.overlay {
+		background: linear-gradient(180deg, rgba(0, 0, 0, 0) 34.98%, #000000 89.97%);
+	}
+</style>
