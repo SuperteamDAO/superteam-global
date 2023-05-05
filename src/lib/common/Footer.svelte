d26c9c4219ed19c7ffedaf93994e86fd3c283e0f
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-05-06 04:55:33 +0530

    footer margins fixed

--- src/lib/common/Footer.svelte
+++ src/lib/common/Footer.svelte
@@ -7,17 +7,20 @@
 	class="footer-container relative left-1/2 right-1/2 -translate-x-1/2 w-screen md:w-[calc(100vw-144px)] h-fit lg:h-[477px] col-span-5 rounded-t-[48px] z-10"
 >
 	<div
-		class="main-container h-fit mt-[100px] lg:mt-[74px] flex lg:flex-row flex-col justify-between"
+		class="main-container h-fit mt-[53px] lg:mt-[56px] flex lg:flex-row flex-col justify-between"
 	>
-		<div class="overlay-logo absolute ml-[40px] mt-[19px] lg:ml-[72px] lg:mt-[17px]">
+		<div class="overlay-logo absolute ml-[50px] mt-[19px] lg:ml-[72px] lg:mt-[17px]">
 			<AnimatedLogo />
 		</div>
 		<div class="footer-logo ml-[40px] lg:ml-[45px]">
 			<div>
 				<img src={superteam_footer} alt="" class="w-3/4 md:w-1/2 lg:w-full" />
 			</div>
-			<div class="overlay-text absolute -mt-[80px] md:-mt-[140px] lg:ml-[27px] lg:mt-0">
-				<p>superteam</p>
+			<div class=" absolute -mt-[80px] md:-mt-[140px] lg:ml-[27px] lg:mt-0">
+				<p class="overlay-text">superteam</p>
+				<p class="text-white mt-4 font-secondary text-[14px] font-bold leading-[15px]">
+					Powered by Solana
+				</p>
 			</div>
 		</div>
 		<div
@@ -27,13 +30,19 @@
 				<a href="https://twitter.com/SuperteamDAO" target="_blank" class="footer-text">Twitter</a>
 				<a href="mailto:hello@superteam.fun" target="_blank" class="footer-text">Email</a>
 				<a href="https://superteam.substack.com/" target="_blank" class="footer-text">Blog</a>
-				<a href="https://www.youtube.com/channel/UCi-pkXLbm7sqXFhV1NBLUfQ" target="_blank" class="footer-text">Podcast</a>
+				<a
+					href="https://www.youtube.com/channel/UCi-pkXLbm7sqXFhV1NBLUfQ"
+					target="_blank"
+					class="footer-text">Podcast</a
+				>
 				<!-- <p class="footer-text mt-4">Earn</p>
 				<p class="footer-text mt-4">Ecosystem Calls</p>
 				<p class="footer-text mt-4">Alpha Squad</p>
 				<p class="footer-text mt-4">Founder's League</p> -->
 			</div>
-			<div class="text-col-2 md:ml-[164px] lg:ml-[123px] md:mt-0 mt-[64px] lg:mb-0 mb-[72px] flex flex-col items-start md:items-end gap-4">
+			<div
+				class="text-col-2 md:ml-[164px] lg:ml-[123px] md:mt-0 mt-[64px] lg:mb-0 mb-[72px] flex flex-col items-start md:items-end gap-4"
+			>
 				<a href="https://in.superteam.fun/" target="_blank" class="footer-text">SuperteamIN</a>
 				<a href="https://de.superteam.fun/" target="_blank" class="footer-text">SuperteamDE</a>
 				<a href="https://tr.superteam.fun/" target="_blank" class="footer-text">SuperteamTR</a>
