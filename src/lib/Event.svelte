2fcef43ea4ee811fa265cbb959be495dffd42f4e
Tushar Choudhari <choudharit23@gmail.com>
2023-06-06 21:29:54 +0530

    Add events

---
+++ src/lib/Event.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { format } from "date-fns"
+    export let event;
+</script>
+
+<div class="min-w-[400px] h-[300px] max-w-[537px] p-6 rounded-xl bg-slate-800 flex flex-col justify-between items-stretch">
+    <div class="flex flex-col">
+        <p class="text-white font-secondary text-[26px] font-bold">
+            {event.fields.Title}
+        </p>
+    </div>
+    <div class="font-secondary flex justify-between text-white text-base">
+        <div class="w-fit py-2 px-3 text-center font-bold bg-black rounded-full">
+            {format(new Date(event.fields.Date), "dd.M.yyyy")}
+        </div>
+        <div class="w-fit py-2 px-3 text-center font-bold bg-black rounded-full">
+            {event.fields.Geo[0]}
+        </div>
+    </div>
+</div>
---
+++ src/lib/Events.svelte
@@ -0,0 +1,44 @@
+<script>
+    import clsx from "clsx";
+	import Event from "./Event.svelte";
+    export let events;
+
+    console.log(events);
+</script>
+
+<div
+	class={clsx(
+		'respect-container h-fit md:h-fit mt-[224px] col-span-5 relative left-1/2 right-1/2 -translate-x-1/2 w-screen overflow-x-visible'
+	)}
+>
+    <div class="love-header">
+        <p class="text-[24px] text-center md:text-[32px] lg:leading-[35px] leading-[26px] section-heading">
+            Superteam IRL
+        </p>
+    </div>
+
+    <div class="flex w-full flex-col gap-6 mt-20">
+        <div class="w-full grid grid-cols-1 scrollbar-hidden">
+            <div class="h-[300px] flex flex-row gap-6">
+                {#each events as event}
+                    <Event event={event} />
+                {/each}
+            </div>
+        </div>
+        <div class="w-full grid greid-cols-1">
+            <div class="h-[300px] flex flex-row gap-6">
+                {#each events as event}
+                    <Event event={event} />
+                {/each}
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .respect-container {
+		justify-content: center;
+		z-index: 4;
+	}
+
+</style>
