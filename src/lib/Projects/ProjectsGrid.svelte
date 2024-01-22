80a55b9ab88f5f4cf31d833bc7454ac256811ae6
Kunal Bagaria <kb24x7@gmail.com>
2024-01-22 16:02:33 +0530

    add: filter ui

--- src/lib/Projects/ProjectsGrid.svelte
+++ src/lib/Projects/ProjectsGrid.svelte
@@ -1,66 +1,87 @@
 <script>
 	import ProjectCard from './ProjectCard.svelte';
+	import { writable, derived } from 'svelte/store';
 
-    export let projects;
+	export let projects = [];
 
-    projects.sort((a, b) => a.fields['Rank'] - b.fields['Rank']);
+	// Sorting projects if it's an array
+	if (Array.isArray(projects)) {
+		projects.sort((a, b) => a.fields['Rank'] - b.fields['Rank']);
+	}
 
-    let offsetArray = (new Array(projects?.length ?? 0)).fill(false);
-    offsetArray[1] = true;
+	// Handling offsetArray logic
+	let offsetArray = new Array(projects.length).fill(false);
+	offsetArray[1] = true;
+	for (let i = 2; i < projects.length; i++) {
+		offsetArray[i] = (i - 1) % 3 === 0;
+	}
 
-    for (let i = 2; i < projects?.length ?? 0; i++) {
-        if ((i - 1) % 3 === 0) {
-            offsetArray[i] = true;
-        } else {
-            offsetArray[i] = false;
-        }
-    }
+	function getOffsetClasses(index) {
+		if (offsetArray[index] && index % 2 === 0) {
+			return 'mt-0 md:mt-8';
+		}
+		if (offsetArray[index] && index % 2 === 1) {
+			return 'mt-0 lg:mt-8';
+		}
+		if (!offsetArray[index] && index % 2 === 0) {
+			return 'mt-0 md:mt-8 lg:mt-0';
+		}
+		return 'mt-0';
+	}
 
-    const getOffsetClasses = (index) => {
-        if (offsetArray[index] && index % 2 === 0) {
-            return 'mt-0 md:mt-8';
-        }
+	// Creating countries array
+	let countries = [];
+	if (Array.isArray(projects)) {
+		countries = Array.from(new Set(projects.map((project) => project.fields['Country'])));
+	}
 
-        if (offsetArray[index] && index % 2 === 1) {
-            return 'mt-0 lg:mt-8';
-        }
+	let selectedCountry = writable('');
 
-        if (!offsetArray[index] && index % 2 === 0) {
-            return 'mt-0 md:mt-8 lg:mt-0';
-        }
-
-        return 'mt-0'
-    }
-
+	// Derived store for filtered projects
+	// const filteredProjects = derived(selectedCountry, ($selectedCountry) =>
+	// 	projects.filter(
+	// 		(project) => $selectedCountry === '' || project.fields['Country'] === $selectedCountry
+	// 	)
+	// );
 </script>
+
+<div class="mt-12 z-50">
+	<p class="text-white font-primary font-medium text-center">Filter Country</p>
+	<select class="mt-4" bind:value={$selectedCountry}>
+		<option value="">All Countries 🌐</option>
+		{#each countries as country}
+			<option>{country}</option>
+		{/each}
+	</select>
+</div>
 
 <div class="flex justify-center items-center mt-[42px] z-20">
 	<div class="flex flex-col gap-8 md:grid md:gap-0 cards-container">
-        {#each projects as { fields }, index}
-            <ProjectCard
-                name={fields.Name}
-                tagline={fields.Tagline}
-                projectLink={fields["Project Link"]}
-                twitterLink={fields["Project Twitter"]}
-                imgUrl={fields["Logo Link"]}
-                class="col-span-1 row-span-1 {getOffsetClasses(index)}"
-            />
-        {/each}
+		{#each projects as { fields }, index}
+			<ProjectCard
+				name={fields.Name}
+				tagline={fields.Tagline}
+				projectLink={fields['Project Link']}
+				twitterLink={fields['Project Twitter']}
+				imgUrl={fields['Logo Link']}
+				country={fields['Country']}
+				class="col-span-1 row-span-1 {getOffsetClasses(index)}"
+			/>
+		{/each}
 	</div>
 </div>
 
 <style>
-    @media (min-width: 786px) {
-        .cards-container {
-            grid-template-columns: repeat(2, 1fr);
-            column-gap: 32px;
-        }
-    }
-    @media (min-width: 1024px) {
-        .cards-container {
-            grid-template-columns: repeat(3, 1fr);
-            grid-template-rows: auto;
-        }
-    }
-
+	@media (min-width: 786px) {
+		.cards-container {
+			grid-template-columns: repeat(2, 1fr);
+			column-gap: 32px;
+		}
+	}
+	@media (min-width: 1024px) {
+		.cards-container {
+			grid-template-columns: repeat(3, 1fr);
+			grid-template-rows: auto;
+		}
+	}
 </style>
