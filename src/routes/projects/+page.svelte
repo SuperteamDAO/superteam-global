366beaf417127c5b465733ae6d48dc30dc0894cb
Syed Aabis Akhtar <74103155+GitBolt@users.noreply.github.com>
2024-01-27 13:36:48 +0530

    fix: dropdown not closing on outside click (#45)

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -31,8 +31,8 @@
 	sizes="(max-width: 640px) 100vw, (max-width: 1440px) 100vw, 2560px"
 	loading="eager"
 	alt=""
+	style="height: 100%;"
 	class="absolute top-0 left-0 w-full object-cover -mt-[72px]"
-  style="height: 100%"
 />
 
 <section class="relative col-span-5 flex flex-col items-center pb-[300px]">
