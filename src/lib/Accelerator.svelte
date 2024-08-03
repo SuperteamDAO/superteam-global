f42186a829e80888e1401f6c81de5001433906ce
Kunal Bagaria <kb24x7@gmail.com>
2024-08-04 02:12:49 +0530

    update: fast track page

--- src/lib/Accelerator.svelte
+++ src/lib/Accelerator.svelte
@@ -1,84 +1,118 @@
 <script>
 	import PartnerCard from '../lib/Partners/PartnerCard.svelte';
-  import virtalIcon from '../assets/fast-tracks/virtualicon.svg';
-  import hybridIcon from '../assets/fast-tracks/hybridicon.svg';
+	import virtalIcon from '../assets/fast-tracks/virtualicon.svg';
+	import hybridIcon from '../assets/fast-tracks/hybridicon.svg';
 
-  import OrangeDao from '../assets/fast-tracks/orangedao.svg';
-  import AllianceDao from '../assets/fast-tracks/alliance.svg';
-  import OutlierVentures from '../assets/fast-tracks/outlier.svg';
-  import Colosseum from '../assets/fast-tracks/colosseum.svg';
-  import SpringX from '../assets/fast-tracks/spring.svg';
-  import SolanaIncubator from '../assets/fast-tracks/solanaincubator.svg';
-
+	import OrangeDao from '../assets/fast-tracks/orangedao.svg';
+	import AllianceDao from '../assets/fast-tracks/alliance.svg';
+	import OutlierVentures from '../assets/fast-tracks/outlier.svg';
+	import Colosseum from '../assets/fast-tracks/colosseum.svg';
+	import SpringX from '../assets/fast-tracks/spring.svg';
+	import SolanaIncubator from '../assets/fast-tracks/solanaincubator.svg';
 
 	const partners = [
 		{
 			imageUrl: OrangeDao,
 			name: 'OrangeDAO',
-			description: 'Special Sign up sheet + expedited reviews for Orange DAO Fellowships',
+			description: 'Orange DAO is building the digital nation of founders helping each other.',
 			iconUrl: virtalIcon,
-			url: 'https://www.orangedao.xyz/'
+			url: 'https://www.orangedao.xyz/',
+			perks: 'Special Sign up sheet + expedited reviews for Orange DAO Fellowships',
+			duration: '12 weeks',
+			funding: '$100k',
+			type: 'Virtual'
 		},
 		{
 			imageUrl: AllianceDao,
 			name: 'Alliance DAO',
-			description: 'Personalized support from the team + feedback on application drafts, strategic advice for startup narrative, guidance on application strategy & interview preparation.',
-      iconUrl: hybridIcon,
-      url: 'https://www.alliance.xyz/'
+			description: 'Alliance is the leading crypto accelerator & founder community.',
+			iconUrl: hybridIcon,
+			url: 'https://www.alliance.xyz/',
+			perks:
+				'Personalized support from the team + feedback on application drafts, strategic advice for startup narrative, guidance on application strategy & interview preparation.',
+			duration: '4 in NY office + 8 remote',
+			funding: '$350k',
+			type: 'Hybrid'
 		},
 		{
 			imageUrl: OutlierVentures,
-			name: 'Outlier Ventures Base camp',
-			description: 'Comment "Superteam" in the referral section of the application to get direct exposure to the Outlier investment team & receive an expedient review on your submission.',
+			name: 'Outlier Ventures',
+			description:
+				'A leading web3 accelerator, with 300+ portfolio companies & supporting founders 360°.',
 			iconUrl: virtalIcon,
-			url: 'https://www.outlierventures.io/base-camp/'
+			url: 'https://www.outlierventures.io/base-camp/',
+			perks:
+				'When applying comment "Superteam" in the referral section at the end of your application to get direct exposure to the Outlier investment team and receive an expedient review on your submission.',
+			duration: '12 weeks',
+			funding: 'upto $200k',
+			type: 'Virtual'
 		},
 		{
 			imageUrl: Colosseum,
 			name: 'Colosseum',
 			description:
-				'Get invited to a special Zoom call, get competition strategies & project submission tips. Chance to get fast tracked into Colosseum if recommended by a Superteam Lead!',
+				"An organization running Solana's hackathons, accelerating select winners, and investing in their breakout startups.",
 			iconUrl: hybridIcon,
-			url: 'https://www.colosseum.org/'
+			url: 'https://www.colosseum.org/',
+			perks:
+				'Get invited to a special Zoom call, get competition strategies & project submission tips. Chance to get fast tracked into Colosseum if recommended by a Superteam Lead!',
+			duration: '6 week hackathon + 6 week accelerator',
+			funding: '$250k',
+			type: 'Hybrid'
+		},
+		{
+			imageUrl: SolanaIncubator,
+			name: 'Solana Incubator',
+			description:
+				'The Solana Incubator is an in-person 3 month program where founders can build their companies alongside Solana Labs.',
+			iconUrl: hybridIcon,
+			url: 'https://incubator.solanalabs.com/',
+			perks:
+				'Get invited to a special Zoom call, get competition strategies & project submission tips. Chance to get fast tracked into Colosseum if recommended by a Superteam Lead!',
+			duration: '3 months',
+			funding: '-',
+			type: 'Hybrid'
 		},
 		{
 			imageUrl: SpringX,
 			name: 'SpringX',
 			description:
-				'Get recommended to the SpringX Accelerator Program at the Solana APAC Summit in Malaysia in June 2024.',
+				'Initiated by BuilderDAO, SuperteamDAO, SevenX Ventures, & ABCDE Labs- it supports early-stage entrepreneurs with education, funding, & resources.',
 			iconUrl: virtalIcon,
-			url: 'https://www.springx.net/'
-		},
-		{
-			imageUrl: SolanaIncubator,
-			name: 'Solana Incubator',
-			description: 'Get invited to a special Zoom call + a chance to get fast tracked into the Solana incubator programm if recommended by a Superteam Lead!',
-			iconUrl: hybridIcon,
-			url: 'https://incubator.solanalabs.com/'
+			url: 'https://www.springx.net/',
+			perks:
+				'Best teams get recommended to the SpringX Accelerator Program at the Solana APAC Summit in Malaysia in June 2024.',
+			duration: '3 weeks offline + 6 month online follow up',
+			funding: 'Conduct IRL workshops, mentoring sessions & investor meetups',
+			type: 'Virtual'
 		}
 	];
 </script>
 
 <section class="text-center text-white flex flex-col justify-center items-center">
 	<div class="bg-image">
-    <div class="partners-content">
-      <p class="header-text">Accelerator Partners</p>
-      <p class="description-text">
-        When applying to any program, let your local Superteam lead know. This allows <br class="description-break" />Superteam
-        to individually support you in getting into an acceleration program.
-      </p>
-      <div class="partners-grid">
-        {#each partners as partner}
-          <PartnerCard
-            imageUrl={partner.imageUrl}
-            name={partner.name}
-            description={partner.description}
-            iconUrl={partner.iconUrl}
-            url={partner.url}
-          />
-        {/each}
-      </div>
-    </div>
+		<div class="partners-content">
+			<p class="header-text">Ecosystem Partners</p>
+			<p class="description-text">
+				When applying to any program, let your local Superteam lead know. This allows <br
+					class="description-break"
+				/>Superteam to individually support you in getting into an acceleration program.
+			</p>
+			<div class="partners-grid">
+				{#each partners as partner}
+					<PartnerCard
+						imageUrl={partner.imageUrl}
+						name={partner.name}
+						description={partner.description}
+						url={partner.url}
+						perks={partner.perks}
+						duration={partner.duration}
+						funding={partner.funding}
+						type={partner.type}
+					/>
+				{/each}
+			</div>
+		</div>
 	</div>
 </section>
 
@@ -87,22 +121,23 @@
 		background-image: url('../assets/fast-tracks/BgGradient.png');
 		background-size: contain;
 		background-position: center;
+		background-repeat: no-repeat;
 		z-index: 1;
 		margin-bottom: 8rem;
 	}
-  @media only screen and (max-width: 1048px) {
-    .bg-image {
-      background: none;
-    }
-  }
-  @media only screen and (max-width: 700px) {
-    .description-break {
-      display: none;
-    }
-  }
-  .partners-content {
-    padding: 0rem 16rem;
-  }
+	@media only screen and (max-width: 1048px) {
+		.bg-image {
+			background: none;
+		}
+	}
+	@media only screen and (max-width: 700px) {
+		.description-break {
+			display: none;
+		}
+	}
+	.partners-content {
+		padding: 0rem 16rem;
+	}
 	.header-text {
 		color: #fff;
 		text-align: center;
@@ -127,16 +162,17 @@
 		margin-bottom: 3.75rem;
 	}
 	.partners-grid {
-		display: grid;
+		display: flex;
+		flex-direction: column;
 		gap: 1.5rem;
 		justify-content: center;
-    justify-items: center;
-		grid-template-columns: repeat(3, 1fr);
-		@media screen and (max-width: 1024px) {
+		justify-items: center;
+		/* grid-template-columns: repeat(3, 1fr); */
+		/* @media screen and (max-width: 1024px) {
 			grid-template-columns: repeat(2, 1fr);
 		}
 		@media screen and (max-width: 700px) {
 			grid-template-columns: repeat(1, 1fr);
-		}
+		} */
 	}
 </style>
