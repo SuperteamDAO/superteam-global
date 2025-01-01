5ee95ee91f0cae9519bf24a159d3d736fde13eba
superteamfun <hello@superteam.fun>
2025-01-02 00:20:57 +0530

    Revert "fix: improve grant partner layout and spacing"
    This reverts commit 12742484538c282bf734d247b7df2bd1413f7845.

--- src/lib/Partners.svelte
+++ src/lib/Partners.svelte
@@ -8,11 +8,11 @@
 
 <div class="partners-container col-span-5 w-full flex flex-col items-center mt-[224px]">
 	<h2
-		class="partners-text section-heading font-secondary text-center font-bold text-white mb-12 mr-[71px] ml-[71px] text-[24px] md:text-[32px] lg:leading-[26px] leading-[22px]"
+		class="partners-text section-heading font-secondary text-center font-bold text-white mb-4 mr-[71px] ml-[71px] text-[24px] md:text-[32px] lg:leading-[26px] leading-[22px]"
 	>
 		Grant Partners
 	</h2>
-	<!-- <div class="logos-container flex flex-wrap justify-center items-center mt-6 gap-11 w-full">
+	<div class="logos-container flex flex-wrap justify-center items-center mt-6 gap-11 w-full">
 		<a href="https://solana.org/" target="_blank">
 			<img class="logo foundation h-6 w-auto" src={foundation} alt="Foundation" loading="lazy" />
 
@@ -26,32 +26,10 @@
 
 		<a href="https://www.circle.com/" target="_blank">
 			<img class="logo circle h-8 w-auto" src={circle} alt="Circle" loading="lazy" />
-		</a> -->
+		</a>
 
-		<div class="flex flex-col items-center w-full">
-			<div class="grid grid-cols-2 md:grid-cols-4 gap-16 w-full max-w-4xl mx-auto px-8">
-			  <a href="https://solana.org/" class="flex justify-center">
-				<img class="h-8" src={foundation} alt="Foundation" loading="lazy" />
-			  </a>
-			  <a href="https://www.jup.ag/" class="flex justify-center">
-				<img class="h-8" src={jupiter} alt="Jupiter" loading="lazy" />
-			  </a>
-			  <a href="https://wormhole.com/" class="flex justify-center">
-				<img class="h-8" src={wormhole} alt="Wormhole" loading="lazy" />
-			  </a>
-			 
-			  <a href="https://www.circle.com/" class="flex justify-center">
-				<img class="h-8" src={circle} alt="Circle" loading="lazy" />
-			  </a>
-			</div>
-		   </div>
-		
-	</div> 
-
-	
-
-  
-
+	</div>
+</div>
 
 <style>
 	.partners-text {
