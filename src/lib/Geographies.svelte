d7d1bcb175ee3df3ea1d992578f12d7751171fcb
superteamfun <hello@superteam.fun>
2025-01-02 06:34:11 +0530

    chore: added more superteam chapters

--- src/lib/Geographies.svelte
+++ src/lib/Geographies.svelte
@@ -8,6 +8,9 @@
 	import uae from '../assets/chapters/uae.webp';
 	import nigeria from '../assets/chapters/nigeria.webp';
 	import balkan from '../assets/chapters/balkan.webp';
+	import malaysia from '../assets/chapters/malaysia.webp';
+	import japan from '../assets/chapters/japan.webp';
+	import france from '../assets/chapters/france.webp';
 	import canada from '../assets/chapters/canada.webp';
 	import singapore from '../assets/chapters/singapore.webp';
 	
@@ -15,10 +18,13 @@
 	  { key: 'india', name: 'India', path: india, href: 'https://in.superteam.fun/' },
 	  { key: 'vietnam', name: 'Vietnam', path: vietnam, href: 'https://vn.superteam.fun/' },
 	  { key: 'germany', name: 'Germany', path: germany, href: 'https://de.superteam.fun/' },
-	  { key: 'uk', name: 'United Kingdom', path: uk, href: 'https://uk.superteam.fun/' },
-	  { key: 'uae', name: 'United Arab Emirates', path: uae, href: 'https://uae.superteam.fun/' },
+	  { key: 'uk', name: 'UK', path: uk, href: 'https://uk.superteam.fun/' },
+	  { key: 'uae', name: 'UAE', path: uae, href: 'https://uae.superteam.fun/' },
 	  { key: 'nigeria', name: 'Nigeria', path: nigeria, href: 'https://ng.superteam.fun/' },
 	  { key: 'balkan', name: 'Balkan', path: balkan, href: 'https://blkn.superteam.fun/' },
+	  { key: 'malaysia', name: 'Malaysia', path: malaysia, href: 'https://my.superteam.fun/' },
+	  { key: 'france', name: 'France', path: france, href: 'https://fr.superteam.fun/' },
+	  { key: 'japan', name: 'Japan', path: japan, href: 'https://jp.superteam.fun/' },
 	  { key: 'singapore', name: 'Singapore', path: singapore, href: 'https://sg.superteam.fun/' },
 	  { key: 'canada', name: 'Canada', path: canada, href: 'https://ca.superteam.fun/' }
 	];
@@ -42,7 +48,7 @@
 	<div class="col-span-5 mt-10]">
 	  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 px-4 sm:px-8 md:px-16">
 		{#each displayedCountries as country}
-		  <a href={country.href} target="_blank" class="group relative aspect-[4/3] overflow-hidden rounded-lg bg-black max-w-[500px] mx-auto">
+		  <a href={country.href} target="_blank" class="group relative aspect-[4/3] overflow-hidden rounded-lg bg-black max-w-[450px] mx-auto">
 			<img src={country.path} alt={country.name} class="w-full h-full object-cover opacity-80 transition-all duration-300 group-hover:opacity-60 group-hover:scale-105" />
 			<h3 class="absolute bottom-6 left-6 text-white text-2xl font-bold font-secondary">{country.name}</h3>
 		  </a>
